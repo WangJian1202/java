2635d88cccb5b7b4488e17908a1cb2e9f12cdfe4
WangJian <1202wjwj>
2018-01-27 20:57:54 +0800

    提交本地代码到github练习

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+import java.security.ProtectionDomain;
+public class HelloWorld {
+    public static void main(String[]args){
+        for (int i = 0; i < 10; i++) {
+            System.out.println("HelloWorld");
+        }
+    }
+
+}
